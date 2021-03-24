690801d3a1888fe7a562b6abbd36733140b38438
Sheraz Siddiqui <sherazsiddiqui@Sherazs-MacBook-Pro.local>
2021-03-24 09:43:55 +0500

    formik with react memo added

--- client/src/components/Input.js
+++ client/src/components/Input.js
@@ -1,53 +1,26 @@
 import React from "react";
 
 const InputField = (props) => {
-  const { name, value, placeholder, inputHandler, type } = props;
+  const { name } = props;
   console.log(`${name} is rendering`);
   return (
     <input
       autoComplete={"off"}
-      name={name}
-      value={value}
-      placeholder={placeholder}
-      onChange={inputHandler}
       className="w-full	mr-3 sm:mt-3 sm:mb-3 rounded shadow-md border-transparent focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-transparent"
-      type={type}
+      {...props}
     />
   );
 };
 
 const MemoInputField = React.memo((props) => {
-  const { name, value, placeholder, inputHandler, type } = props;
-
-  // const [emailM, setEmailM] = React.useState("");
-
-  // const inputHandlerM = (event) => {
-  //   try {
-  //     const { name, value } = event.target;
-  //     setEmailM(value);
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // };
-
+  const { name } = props;
   console.log(`${name} is rendering -> memo`);
   return (
     <input
       autoComplete={"off"}
-      name={name}
-      value={value}
-      placeholder={placeholder}
-      onChange={inputHandler}
       className="w-full	mr-3 sm:mt-3 sm:mb-3 rounded shadow-md border-transparent focus:outline-none focus:ring-2 focus:ring-blue-300 focus:border-transparent"
-      type={type}
+      {...props}
     />
-    //  <input
-    // autoComplete={"off"}
-    //   name={"emailM"}
-    //   value={emailM}
-    //   placeholder={"email address"}
-    //   onChange={inputHandlerM}
-    // />
   );
 });
 
